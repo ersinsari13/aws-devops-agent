d68c74ceb6fd5371c7491d093bd5934dd5352d94
ersinsari13 <sariiersinn13@gmail.com>
2025-12-28 14:44:15 +0300

    update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 import os
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # Burası şu an düzgün çalışıyor
-    return "Merhaba! EKS uzerinde Calisan v1 Uygulamasi. Her sey yolunda!", 200
+    return "Merhaba! Bu v2 versiyonu.", 200
+
+
+print("Uygulama baslatiliyor... Konfigurasyonlar yukleniyor...")
+
+print("KRITIK HATA: Veritabani baglantisi kurulamadi! (Timeout)")
+
+raise Exception("FATAL ERROR: Database Connection Failed in deployment v2")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
